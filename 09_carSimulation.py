e67e9a4286b2dc121a895459022e0a5a961ff1f8
prasanna <16790107+Prasan1@users.noreply.github.com>
2020-03-24 21:38:11 -0400

    Update 09_carSimulation.py

--- 09_carSimulation.py
+++ 09_carSimulation.py
@@ -2,11 +2,6 @@
 #!/usr/bin/env python
 
 '''This program simulates a car engine.
-   This is a part assignment in Mosh python tutorial.
-   You can visit his website below: 
-   https://codewithmosh.com/p/python-programming-course-beginners/?product_id=866084&coupon_code=YOUTUBE&preview=logged_out
-   To watch his youtube video please follow the following link:
-   https://www.youtube.com/watch?v=_uQrJ0TkZlc
 '''
 
 # user input
